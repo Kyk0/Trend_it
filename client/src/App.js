6ce008f2c43198d3debf36d75a331858eaf6b1bf
Kyrylo Kolesnichenko <kykocq@gmail.com>
2024-12-04 02:55:53 +0200

    web analytics 3

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
-import {Analytics} from "@vercel/analytics/dist/react/index";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
