193c6d25a9ba44c33a00ec721bd1a11fcd84ee7d
Kyrylo Kolesnichenko <kykocq@gmail.com>
2024-12-04 02:51:50 +0200

    web analytics

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
+import {Analytics} from "@vercel/analytics/dist/react/index";
 
 const App = () => {
   return (
       <Router>
         <Navbar />
-        <Routes>
+            <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<div className="text-white">About Page</div>} />
           <Route path="/contact" element={<div className="text-white">Contact Page</div>} />
-        </Routes>
+            </Routes>
+          <Analytics />
       </Router>
   );
 };
